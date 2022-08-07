26af37f19a97cccb006ae11687a9a27e782fd1d4
kareynolds <ka.reynolds@outlook.com>
2022-08-06 19:04:34 -0700

    add psuedo code to work out needed logic

---
+++ script.js
@@ -0,0 +1,9 @@
+// when you click the button | add an onclick event listener to the html. store it in a variable. 
+
+//grab the number in the input. assign the number to a variable. 
+
+//perform some sort of math with the number, store the math in a result.
+
+//grab the elementbyid for each unit conversion and store it in a variable
+
+//edit the p tag by adding the conversion from the return of the mathh. 
