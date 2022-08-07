0178d226fef8161bcf7810a295e70f9562fb6d3e
kareynolds <ka.reynolds@outlook.com>
2022-08-06 20:47:12 -0700

    add onclick event listener to button
    add math to convert length
    
    working on updating the dom with correct conversions

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // when you click the button | add an onclick event listener to the html. store it in a variable. 
 
-//grab the number in the input. assign the number to a variable. 
+//create the onclick function which grabs the number in the input. assign the number to a variable. 
+
+
+function convertBtn() {
+    let convertUnit = document.getElementById("input-field").value
+
+    function convertLength() {
+        // 1 meter = 3.281 feet
+        let meters = convertUnit * 3.281
+        let feet = convertUnit * .03048 * 10
+
+        let lengthConverted = document.getElementById("length-converted");
+    
+        console.log(meters);
+        console.log(feet);
+        
+    }
+    convertLength() 
+    
+    function convertVolume() {
+    
+    }
+    
+    function convertMass() {
+    
+    }
+    
+
+    console.log(convertUnit);
+}
+
+
 
 //perform some sort of math with the number, store the math in a result.
 
